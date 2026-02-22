26bd7a84b832f0e14ddd3ac13e2b0e77d8b14e4c
MiMics0 <96246790+MiMics0@users.noreply.github.com>
2026-02-23 03:41:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -237,7 +237,7 @@ client.once("clientReady", async () => {
   }
 
   /* ================= 23:59 ปิดรับ + จับคู่ ================= */
-  cron.schedule("* * * * *", async () => {
+  cron.schedule("59 23 * *", async () => {
     const data = loadData();
 
     data.statusClosed = true;
@@ -347,5 +347,6 @@ client.on("interactionCreate", async interaction => {
 });
 
 client.login(TOKEN);
+
 
 
